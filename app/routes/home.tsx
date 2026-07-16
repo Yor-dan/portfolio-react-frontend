b97e70b41c0bba9d19cac89d32fcb40eac43d19f
Yordan Trivada Bian <yordanbian@gmail.com>
2026-07-16 23:46:48 +0700

    implemented data fetching from Strapi backend for the Hero component.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,6 @@
 import type { Route } from './+types/home';
+import { useLoaderData } from 'react-router';
+import Hero from '~/components/Hero';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,4 +12,29 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {}
+export async function loader() {
+  const res = await fetch(`${process.env.STRAPI_URL}/api/her`);
+
+  if (!res.ok) {
+    console.error('Error fetching hero data:', res.statusText);
+  }
+
+  const payload = await res.json();
+  const hero = payload?.data || {};
+
+  return {
+    title: hero.title || 'Yordan Bian',
+    subtitle: hero.subtitle || 'A Software Engineer',
+    eyebrow: hero.eyebrow || null,
+  };
+}
+
+export default function Home() {
+  const { title, subtitle, eyebrow } = useLoaderData<typeof loader>();
+
+  return (
+    <div className="hero">
+      <Hero {...{ title, subtitle, eyebrow }} />
+    </div>
+  );
+}
