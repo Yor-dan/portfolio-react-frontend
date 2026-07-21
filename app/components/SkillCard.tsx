5399ce360250f18874a5ab6ed45c5b3b1b3a6fa7
Yordan Trivada Bian <yordanbian@gmail.com>
2026-07-21 16:42:42 +0700

    create SkillCard component and Skills loader

---
+++ app/components/SkillCard.tsx
@@ -0,0 +1,24 @@
+export type SkillCardProps = {
+  name: string;
+  cdn: string;
+};
+
+export async function SkillsLoader() {
+  try {
+    const res = await fetch(`${process.env.STRAPI_URL}/api/skills`);
+    const payload = await res.json();
+    return payload.data;
+  } catch (error) {
+    console.error('Error fetching "Skills" data:', error);
+    return [];
+  }
+}
+
+export default function SkillCard({ name, cdn }: SkillCardProps) {
+  return (
+    <div className="skill-card">
+      <img src={cdn} alt={`${name} Icon`} />
+      <h3>{name}</h3>
+    </div>
+  );
+}
